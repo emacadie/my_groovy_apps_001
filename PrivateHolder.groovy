b54b504b0b8ea23c5edef30ebd074c536fa61e2f
Eric MacAdie <Eric@MacAdie.net>
2014-03-15 16:23:57 -0500

    Added PrivateHolder to work with private variables

---
+++ PrivateHolder.groovy
@@ -0,0 +1,15 @@
+class PrivateHolder {
+    
+    def firstString
+    def reverseString
+    
+    def setReverseString( arg ) {}
+    
+    // public PrivateHolder() {}
+    
+    def performStringReversal() {
+        reverseString = firstString.reverse()
+    }
+    
+}
+
