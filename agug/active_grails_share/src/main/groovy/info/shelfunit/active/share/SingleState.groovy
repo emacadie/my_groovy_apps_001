76b59c9b6b93221f934d6748f982675aa73e0e3a
Eric MacAdie <Eric@MacAdie.net>
2014-04-16 23:33:53 -0500

    I found out how to add some scripts to the distribution in gradle

---
+++ agug/active_grails_share/src/main/groovy/info/shelfunit/active/share/SingleState.groovy
@@ -0,0 +1,15 @@
+package info.shelfunit.active.share
+
+import org.javalite.activejdbc.Model
+import org.javalite.activejdbc.annotations.Table
+
+@Table( "single_state" )
+class SingleState extends Model {
+
+  static{ 
+    validatePresenceOf( "state_name" ).message( "Please, provide the state's name" )
+    validatePresenceOf( "largest_city" ).message( "Please, provide the name of the state's largest city" )
+    validatePresenceOf( "capital_city" ).message( "Please, provide the name of the state's capital city" )
+  }    
+
+}
