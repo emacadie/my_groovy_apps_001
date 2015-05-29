405d61ff5f3bf010a7c4f1b84cc06e531c933817
EMacAdie <EMacAdie@gmail.com>
2015-05-28 21:52:09 -0500

    Cleaning up comments

--- programming_groovy2/src/main/groovy/info/shelfunit/venkat/ch07/BetterThreadExample.groovy
+++ programming_groovy2/src/main/groovy/info/shelfunit/venkat/ch07/BetterThreadExample.groovy
@@ -3,34 +3,31 @@ package info.shelfunit.venkat.ch07
 class BetterThreadExample {
     
     def printThreadInfo( msg ) {
-        // def currentThread = Thread.currentThread()
-        println "$msg" // Thread is ${currentThread}. Daemon? ${currentThread.isDaemon()}"
+        println "$msg" 
     }
     
     def doStuff() {
-
-        sleep(5000)
+        sleep( 5000 )
         printThreadInfo 'Main'
         Thread.start {
-            // def name = 
             printThreadInfo "AA Started"
-            sleep(3000) 
+            sleep( 3000 ) 
             println "AA Interrupted 1" 
             sleep( 1000 )
             println "AA IN started"
-            sleep (3000)
+            sleep( 3000 )
             println "AA still in started"
             println "AA Finished Started"
         }
-        // sleep(1000)
+        
         Thread.start {
             printThreadInfo "BB Started Daemon"
-            sleep(5000) 
+            sleep( 5000 ) 
             println "BB Interrupted 2" 
-            sleep(1000)
+            sleep( 1000 )
             println "BB in second thread"
-            sleep(1000)
-            println "BB Finished Started Daemon" // Will not get here
+            sleep( 1000 )
+            println "BB Finished Started Daemon"
         }
     }
       static void main( String[] args ) { 
