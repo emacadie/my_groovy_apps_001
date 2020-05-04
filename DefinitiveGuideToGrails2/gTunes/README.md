df7b6316da206df16629286a5e82435450ede35e
Eric MacAdie <eric@MacAdie.net>
2020-05-04 01:12:39 -0500

    Added DefinitiveGuideToGrails2

---
+++ DefinitiveGuideToGrails2/gTunes/README.md
@@ -0,0 +1,5 @@
+I am going to go through the book "Definitive Guide to Grails 2" by Jeff Scott Brown and Graeme Rocher  
+
+  
+
+
---
+++ DefinitiveGuideToGrails2/gTunes/grails-app/domain/com/gtunes/Song.groovy
@@ -0,0 +1,17 @@
+package com.gtunes
+
+class Song { 
+
+  String title
+  String artist
+  Album album
+
+
+  static constraints = { 
+    title blank: false
+    artist blank: false
+  }
+
+}
+
+
